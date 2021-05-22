87b0eac9f3a0cfaab6f6a2f15a5bcdb65946925f
DANG Truong <dangtruong080593@gmail.com>
2021-05-22 19:26:08 +0700

    refactor linked list to prepare for implementation of stack

--- list_create_insert_remove.f90
+++ list_create_insert_remove.f90
@@ -3,34 +3,34 @@ SUBROUTINE list_create_insert_remove
 	USE improved_linked_list
 	IMPLICIT NONE
 	INTEGER              :: num
-	TYPE (linked_list_t) :: linked_list_demo
+	TYPE (linked_list_t) :: singly_linked_list
 
 	DO num = 1, 5
 		IF (num == 1) THEN 
-			CALL create_head(linked_list_demo, num)
+			CALL create_head(singly_linked_list, num)
 		ELSE
-			CALL append_tail(linked_list_demo,num)
+			CALL append_tail(singly_linked_list,num)
 		ENDIF
 	ENDDO
 
 	PRINT *, 'Transverse the list built up and print the values'
 
-	CALL print_list(linked_list_demo)
+	CALL print_list(singly_linked_list)
 
 	PRINT *, 'Insert new node with value is 10 after node 3 ...'
 
-	CALL append_after_node(linked_list_demo, 3, 10)
+	CALL append_after_node(singly_linked_list, 3, 10)
 
 	PRINT *, 'Transverse the list built up and print the values'
 
-	CALL print_list(linked_list_demo)
+	CALL print_list(singly_linked_list)
 
 	PRINT *, 'Remove node has value 10 ...'
 
-	CALL remove_node(linked_list_demo, 10)
+	CALL remove_node(singly_linked_list, 10)
 
 	PRINT *, 'Transverse the list built up and print the values'
 
-	CALL print_list(linked_list_demo)
+	CALL print_list(singly_linked_list)
 
 END SUBROUTINE
--- list_selection_sort.f90
+++ list_selection_sort.f90
@@ -2,26 +2,26 @@ SUBROUTINE list_selection_sort
 	USE linked_list
 	USE improved_linked_list
 	IMPLICIT NONE
-	TYPE (linked_list_t) :: linked_list_demo
+	TYPE (linked_list_t) :: singly_linked_list
 
-	CALL create_head(linked_list_demo, 10)
-	CALL append_tail(linked_list_demo, 8)
-	CALL append_tail(linked_list_demo, 6)
-	CALL append_tail(linked_list_demo, 1)
-	CALL append_tail(linked_list_demo, 5)
-	CALL append_tail(linked_list_demo, 2)
-	CALL append_tail(linked_list_demo, 9)
-	CALL append_tail(linked_list_demo, 4)
-	CALL append_tail(linked_list_demo, 7)
-	CALL append_tail(linked_list_demo, 3)
+	CALL create_head(singly_linked_list, 10)
+	CALL append_tail(singly_linked_list, 8)
+	CALL append_tail(singly_linked_list, 6)
+	CALL append_tail(singly_linked_list, 1)
+	CALL append_tail(singly_linked_list, 5)
+	CALL append_tail(singly_linked_list, 2)
+	CALL append_tail(singly_linked_list, 9)
+	CALL append_tail(singly_linked_list, 4)
+	CALL append_tail(singly_linked_list, 7)
+	CALL append_tail(singly_linked_list, 3)
 
 	PRINT *, 'Transverse the list built up and print the values before sorting:'
 
-	CALL print_list(linked_list_demo)
+	CALL print_list(singly_linked_list)
 
-	CALL selection_sort(linked_list_demo)
+	CALL selection_sort(singly_linked_list)
 
 	PRINT *, 'Transverse the list built up and print the values after sorting:'
 
-	CALL print_list(linked_list_demo)
+	CALL print_list(singly_linked_list)
 END SUBROUTINE
